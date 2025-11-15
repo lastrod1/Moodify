b1f355b294a02f28370c4edef3f24cb402f4d60a
lastrod1 <mtran3938@gmail.com>
2025-11-15 11:56:04 -0600

    CHORE: Forgot to commit this with previous commit This commit contains the logic for the signup api

--- backend/moodify_api/auth/routes.py
+++ backend/moodify_api/auth/routes.py
@@ -23,3 +23,32 @@ def login():
     
     else:
         return jsonify({"message": "Invalid username or password"}), 401
+    
+@auth.route('/sign-up', methods=['POST'])
+def signup():
+    data = request.get_json();
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({"message": "Missing username or password"}), 400
+        # I don't think this will ever happen since I put required on the forms but just in case
+    
+    existing_user = Login.query.filter_by(username=username).first()
+    if existing_user:
+        return jsonify({"message": "Username already exists"}), 409
+    
+    new_user = Login(
+        username = username,
+        password = password
+    )
+
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({"message": "User created successfully"}), 201
+    except Exception:
+        db.session.rollback()
+        print(f"Database error during signup: {Exception}")
+        return jsonify({"message": "An internal error occured."}), 500
